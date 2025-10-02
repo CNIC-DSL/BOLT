4b0e9ad269be3f444b1b4f667fdcf3b7570a7295
lijinpeng <2073944497@qq.com>
2025-10-02 00:07:32 +0000

    modify cli_openset

--- cli_openset.py
+++ cli_openset.py
@@ -21,7 +21,10 @@ def _maybe(v, flag: str) -> List[str]:
 
 def _epoch_flags(args_json:Dict[str,Any], is_pretrain: bool) -> List[str]:
     """根据阶段返回统一的 epoch 参数"""
-    return ["--num_pretrain_epochs", str(args_json["num_pretrain_epochs"]), "--num_train_epochs", str(args_json["num_train_epochs"])]
+    if is_pretrain:
+        return ["--num_pretrain_epochs", str(args_json["num_pretrain_epochs"]), "--num_train_epochs", str(args_json["num_train_epochs"])]
+    else:
+        return ["--num_train_epochs", str(args_json["num_train_epochs"])]
     
 def _common_flags(args_json: Dict[str, Any]) -> List[str]:
     """
@@ -32,7 +35,7 @@ def _common_flags(args_json: Dict[str, Any]) -> List[str]:
     extra = list(args_json.get("extra_flags", []))
     output_dir = f"outputs/openset/{args_json['method']}/{args_json['dataset']}_{args_json['labeled_ratio']}_{args_json['known_cls_ratio']}_{args_json['fold_idx']}_{args_json['seed']}"
     bert_model = "./pretrained_models/bert-base-chinese" if args_json["dataset"] in ['ecdt', 'thucnews'] else "./pretrained_models/bert-base-uncased"
-    if args_json['method'].lower() in ['ab', 'deepunk', 'doc', 'plm_ood']:
+    if args_json['method'].lower() in ['ab', 'deepunk', 'doc', 'plm_ood', 'clap']:
         return [
             "--config", str(args_json["config"]),
             "--seed", str(args_json["seed"]),
